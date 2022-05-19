feb2fb0cbc0b647aa969f9ab15a4e8a3db615570
moc.kooltuo <>
2022-05-19 14:52:56 -0500

    initial modifications

--- scripts.sh
+++ scripts.sh
@@ -1,5 +1,7 @@
-bash scripts/prepare_dataset.sh
-bash scripts/prepare_mels.sh
+export CUDA_VISIBLE_DEVICES=
+export PYTORCH_JIT=0
+#bash scripts/prepare_dataset.sh
+#bash scripts/prepare_mels.sh
 bash scripts/train_tacotron2.sh
 bash scripts/train_waveglow.sh
 bash scripts/inference.sh
--- scripts/inference.sh
+++ scripts/inference.sh
@@ -1,2 +1,2 @@
 mkdir -p output
-python inference.py --tacotron2 output/checkpoint_Tacotron2_last.pt --waveglow output/checkpoint_WaveGlow_last.pt -i phrases/phrase.txt -o ./output/ --fp16
+python -m Tacotron2 inference.py --tacotron2 output/checkpoint_Tacotron2_last.pt --waveglow output/checkpoint_WaveGlow_last.pt -i phrases/phrase.txt -o ./output/ --log-file inference.json --fp16
