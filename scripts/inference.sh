fa614608e71abd83bf05453bdca67fdd7ab4ce08
moc.kooltuo <>
2022-05-19 14:56:40 -0500

    initial modifications

--- scripts/inference.sh
+++ scripts/inference.sh
@@ -1,2 +1,2 @@
 mkdir -p output
-python -m Tacotron2 inference.py --tacotron2 output/checkpoint_Tacotron2_last.pt --waveglow output/checkpoint_WaveGlow_last.pt -i phrases/phrase.txt -o ./output/ --log-file inference.json --fp16
+python -m multiproc inference.py --tacotron2 output/checkpoint_Tacotron2_last.pt --waveglow output/checkpoint_WaveGlow_last.pt -i phrases/phrase.txt -o ./output/ --log-file inference.json --fp16
