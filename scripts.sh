c55d56fc941295365fa99d6a391fee53d7223bc6
moc.kooltuo <>
2022-05-19 15:53:49 -0500

    initial modifications

--- scripts.sh
+++ scripts.sh
@@ -1,8 +1,12 @@
 #export CUDA_VISIBLE_DEVICES="0,1"
 export CUDA_VISIBLE_DEVICES="0"
 export PYTORCH_JIT=0
+
+rm -r output_old
+mv output output_old
+
 #bash scripts/prepare_dataset.sh
-bash scripts/prepare_mels.sh
+#bash scripts/prepare_mels.sh
 bash scripts/train_tacotron2.sh
 bash scripts/train_waveglow.sh
 bash scripts/inference.sh
