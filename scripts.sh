04317a2a323e40a4ae91af656a34d37a47b7f6d1
moc.kooltuo <>
2022-05-19 16:01:36 -0500

    initial modifications

--- scripts.sh
+++ scripts.sh
@@ -2,7 +2,8 @@
 export CUDA_VISIBLE_DEVICES="0"
 export PYTORCH_JIT=0
 
-rm -r output_old LJSpeech-1.1/mels
+#rm -r LJSpeech-1.1/mels
+rm -r output_old
 mv output output_old
 
 #bash scripts/prepare_dataset.sh
