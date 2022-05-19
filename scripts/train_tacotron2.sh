23d05e386e2eb5249efd5986e54d2a6985f71d55
moc.kooltuo <>
2022-05-19 15:24:20 -0500

    initial modifications

--- scripts/train_tacotron2.sh
+++ scripts/train_tacotron2.sh
@@ -1,2 +1,2 @@
 mkdir -p output
-python -m multiproc train.py -m Tacotron2 -o ./output/ --log-file tacotron.json --seed 0 --resume-from-last --amp --cudnn-enabled --epochs-per-checkpoint 1 --epochs 2 -lr 1e-3 -bs 48 --weight-decay 1e-6 --grad-clip-thresh 1.0 ----anneal-steps 2 4 --anneal-factor 0.1
+python -u -m multiproc train.py -m Tacotron2 -o ./output/ --log-file tacotron.json --seed 0 --resume-from-last --amp --cudnn-enabled --epochs-per-checkpoint 1 --epochs 2 -lr 1e-3 -bs 64 --weight-decay 1e-6 --grad-clip-thresh 1.0 ----anneal-steps 2 4 --anneal-factor 0.1
--- scripts/train_waveglow.sh
+++ scripts/train_waveglow.sh
@@ -1,2 +1,2 @@
 mkdir -p output
-python -m multiproc train.py -m WaveGlow  -o ./output/ --log-file waveglow.json --seed 0 --resume-from-last --amp --cudnn-enabled --epochs-per-checkpoint 1 --epochs 1 -lr 1e-4 -bs 64 --weight-decay 0e-0 --grad-clip-thresh 3.4028234663852886e+38 --segment-length 8000 --wn-channels 128
+python -u -m multiproc train.py -m WaveGlow  -o ./output/ --log-file waveglow.json --seed 0 --resume-from-last --amp --cudnn-enabled --epochs-per-checkpoint 1 --epochs 2 -lr 1e-4 -bs 64 --weight-decay 0e-0 --grad-clip-thresh 3.4028234663852886e+38 --segment-length 8000 --wn-channels 128
