91aa35b857da490453e577e43804e6b97af35ceb
moc.kooltuo <>
2022-05-21 17:20:17 -0500

    initial modifications

--- scripts/train_waveglow.sh
+++ scripts/train_waveglow.sh
@@ -1,2 +1,2 @@
 mkdir -p output
-python -m multiproc -u train.py -m WaveGlow  -o ./output/ --log-file waveglow.json --seed 0 --resume-from-last --amp --cudnn-enabled --filter-length 512 --win-length 512 --hop-length 256 --n-mel-channels 80 --epochs-per-checkpoint 3 --epochs 301 -lr 1e-4 -bs 256 --weight-decay 0e-0 --grad-clip-thresh 3.4028234663852886e+38 --segment-length 4000 --wn-channels 128 --wn-layers 4
+python -m multiproc -u train.py -m WaveGlow  -o ./output/ --log-file waveglow.json --seed 0 --resume-from-last --amp --cudnn-enabled --filter-length 512 --win-length 512 --hop-length 256 --n-mel-channels 80 --epochs-per-checkpoint 3 --epochs 501 -lr 1e-4 -bs 256 --weight-decay 0e-0 --grad-clip-thresh 3.4028234663852886e+38 --segment-length 4000 --wn-channels 128 --wn-layers 4
