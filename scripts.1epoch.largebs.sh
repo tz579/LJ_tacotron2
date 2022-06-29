5a7f418a75f04934b0af83961c858c24db259023
moc.kooltuo <>
2022-06-28 19:25:30 -0500

    initial modifications

---
+++ scripts.1epoch.largebs.sh
@@ -0,0 +1,13 @@
+#export CUDA_VISIBLE_DEVICES="0,1"
+export CUDA_VISIBLE_DEVICES="0"
+export PYTORCH_JIT=0
+
+#rm -r LJSpeech-1.1/mels
+#rm -r output_old
+#mv output output_old
+
+bash scripts/prepare_dataset.sh
+bash scripts/prepare_mels.sh
+bash scripts/train_tacotron2.1epoch.largebs.sh
+bash scripts/train_waveglow.1epoch.largebs.sh
+bash scripts/inference.sh
---
+++ scripts/train_waveglow.1epoch.littlebs.sh
@@ -0,0 +1,2 @@
+mkdir -p output
+python -m multiproc -u train.py -m WaveGlow  -o ./output/ --log-file waveglow.json --seed 0 --resume-from-last --amp --cudnn-enabled --filter-length 512 --win-length 512 --hop-length 256 --n-mel-channels 80 --epochs-per-checkpoint 1 --epochs 1 -lr 1e-4 -bs 48 --weight-decay 0e-0 --grad-clip-thresh 3.4028234663852886e+38 --segment-length 4000 --wn-channels 128 --wn-layers 4
